209079fba148dff0bd362f189eeb65377bb384e7
MathNerdGamer <mathnerd@anothermathblog.com>
2024-05-07 12:15:56 -0700

    Hello, world!

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,7 @@
+// This program prints Hello, World!
+package HelloWorld;
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
---
+++ Notes/Week1.md
@@ -0,0 +1,3 @@
+# Week 1
+* Every program requires a `class` containing a method called `main`.
+* The name of a `class` must be identical to the name of the file.
